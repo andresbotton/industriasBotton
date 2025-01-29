3050d291cfaf4121bef35074db8dbfe44c3b35c0
afbotton <afbotton@misena.edu.co>
2025-01-29 10:04:14 -0500

    new actualization
    primera parte

--- js/PaginaPrincipal.js
+++ js/PaginaPrincipal.js
@@ -11,4 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.getElementById("copyright-year").textContent =
+    new Date().getFullYear();
 
+window.addEventListener("beforeunload", function (event) {
+    localStorage.clear();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector("body").classList.add("loaded");
+    }, 500);
+});
--- js/script2.js
+++ js/script2.js
@@ -9,3 +9,35 @@ window.addEventListener('scroll', function () {
     document.querySelector('.background').style.transform = 'translateY(' + backgroundOffset + 'px)';
     document.querySelector('.layer').style.transform = 'translateY(' + layerOffset + 'px)';
 });
+
+(function(){
+    
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;   
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    });
+
+    const changePosition = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
+
+        sliders[value-1].classList.add('testimony__body--show');
+
+    }
+
+})();
